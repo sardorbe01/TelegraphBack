bad4a87b533fb63ee5c2f66891cb38d46168bc0e
sardorbe01 <134583885+sardorbe01@users.noreply.github.com>
2023-05-25 11:43:54 +0500

    Add files via upload

---
+++ Telegraph/src/main/java/uz/es/telegraph/TelegraphApplication.java
@@ -0,0 +1,13 @@
+package uz.es.telegraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegraphApplication.class, args);
+    }
+
+}
---
+++ Telegraph/src/test/java/uz/es/telegraph/TelegraphApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.es.telegraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegraphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
